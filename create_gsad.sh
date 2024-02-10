e3bee09f225d1eb2666bddd9a6e28ad00f67adec
NunoRomao <86793345+philip17rc@users.noreply.github.com>
2024-02-10 14:27:14 +0000

    Add files via upload

---
+++ create_gsad.sh
@@ -0,0 +1,24 @@
+sudo cp -v $BUILD_DIR/gvmd.service /etc/systemd/system/
+
+cat << EOF > $BUILD_DIR/gsad.service
+[Unit]
+Description=Greenbone Security Assistant daemon (gsad)
+Documentation=man:gsad(8) https://www.greenbone.net
+After=network.target gvmd.service
+Wants=gvmd.service
+
+[Service]
+Type=exec
+User=gvm
+Group=gvm
+RuntimeDirectory=gsad
+RuntimeDirectoryMode=2775
+PIDFile=/run/gsad/gsad.pid
+ExecStart=/usr/local/sbin/gsad --foreground --listen=0.0.0.0 --port=9392 --http-only
+Restart=always
+TimeoutStopSec=10
+
+[Install]
+WantedBy=multi-user.target
+Alias=greenbone-security-assistant.service
+EOF
